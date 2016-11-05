b92558447820d0cd1fe19aa5622e2e5a418c3d0f
tanyonglong <342875368@qq.com>
2016-11-05 12:56:40 +0800

    add files

--- drag/drag.js
+++ drag/drag.js
@@ -1,11 +1,27 @@
+
+/*
+	拖拽
+
+*/
 function dragToSomeWhere( drag, drop) {
-		
+	
 		var draggable = drag.getAttribute("draggable");
+		if( !draggable ){
+			drag.setAttribute("draggable", "true");	
+		}
+		drop.addEventListener("dragover", function (ev) {
+			ev.preventDefault();
+		}, false);
 
+		drag.addEventListener("dragstart", function (ev) {
+			ev.dataTransfer.setData("Text",ev.target.id);
+		}, false)
 
-		alert(drag.draggable);
-
-
-
+		drop.addEventListener("drop", function (ev) {
+			ev.preventDefault();
+			ev.stopPropagation();
+			var data = ev.dataTransfer.getData("Text");
+			ev.target.appendChild(document.getElementById(data));
+		}, false);
 
 }
